4bb5dfa2763c0c0cc849031c315161d1d2e96995
TiMoscow <tim-kap@ya.ru>
2025-03-05 02:43:22 +0300

    v0.2.8: TV-Ready AVI Converter Исправлено: rollback.bat создаётся только при ошибках

--- process_videos.bat
+++ process_videos.bat
@@ -1,5 +1,5 @@
 @echo off
-:: Версия кода: 0.2.6
+:: Версия кода: 0.2.8
 
 setlocal enabledelayedexpansion
 chcp 65001 > nul
@@ -13,16 +13,20 @@ set "rollbackFile=rollback.bat"
 if exist "%logFile%" del "%logFile%"
 if exist "%rollbackFile%" del "%rollbackFile%"
 
+:: Флаг для создания rollback.bat
+set "needRollback=0"
+
 :: Обработка всех AVI-файлов
 for /r %%f in (*.avi) do (
     set "currentFile=%%~f"
     set "backupFile=%%~dpnf.backup.avi"
     echo Обработка: !currentFile!
 
-    :: Создание резервной копии через копирование (вместо переименования)
+    :: Создание резервной копии
     copy "!currentFile!" "!backupFile!" >nul 2>&1
     if not exist "!backupFile!" (
         echo [%date% %time%] [Ошибка] Не удалось создать резервную копию: !currentFile! >> "%logFile%"
+        set "needRollback=1"
         goto :next_file
     )
 
@@ -37,19 +41,19 @@ for /r %%f in (*.avi) do (
         if errorlevel 1 (
             echo [%date% %time%] [Ошибка] FFmpeg не смог обработать файл: !currentFile! >> "%logFile%"
             del "!currentFile!_temp.avi" >nul 2>&1
+            set "needRollback=1"
         ) else (
             :: Замена исходного файла
             move /y "!currentFile!_temp.avi" "!currentFile!" >nul 2>&1
             if exist "!currentFile!" (
                 echo [%date% %time%] [Успех] Обработан: !currentFile! >> "%logFile%"
-                :: Запись в rollback.bat
-                echo if exist "!backupFile!" move /y "!backupFile!" "!currentFile!" >> "%rollbackFile%"
+                :: Удаление резервной копии после успешной обработки
+                del "!backupFile!" >nul 2>&1
             ) else (
                 echo [%date% %time%] [Ошибка] Не удалось заменить файл: !currentFile! >> "%logFile%"
+                set "needRollback=1"
             )
         )
-        :: Удаление резервной копии после успешной обработки
-        del "!backupFile!" >nul 2>&1
     )
 
     :next_file
@@ -57,6 +61,12 @@ for /r %%f in (*.avi) do (
     set "backupFile="
 )
 
+:: Создание rollback.bat только при наличии ошибок
+if !needRollback! EQU 1 (
+    echo Для отката запустите: %rollbackFile%
+) else (
+    if exist "%rollbackFile%" del "%rollbackFile%"
+)
+
 echo Все файлы обработаны. Лог: %logFile%
-echo Для отката запустите: %rollbackFile%
 pause
